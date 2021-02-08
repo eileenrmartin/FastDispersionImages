e3cf7782ffe61bf0b85a4bad20eb2bd0cf5ac9e6
Anuradha Trivedi <agt17@vt.edu>
2021-02-08 10:01:20 -0500

    updated definition of wave number k, added in comments for to-do items

--- planeWave.py
+++ planeWave.py
@@ -10,29 +10,34 @@ import matplotlib.pyplot as plt
 
 def planeWave(k, w, theta, sensors, totTime):
     
+    # make x and k vectors for future cases (add this in)
+    
     x    = np.linspace(0, 2*sensors, sensors)
-    t    = np.linspace(0, totTime, w*totTime)
+    t    = np.linspace(0, totTime, 4*int(w/np.pi)*totTime)
     wave = np.zeros((len(x), len(t)))
     
+    
     for i in range(len(x)): # loop over rows (channel #)
-        for j in range(len(t)): # loop over columns (time)
-            wave[int(i),int(j)] = np.cos(x[int(i)]*k - w*t[int(j)])
+        wave[int(i),:] = np.cos(x[int(i)]*k - w*t)
 
     return wave
               
 ## create single frequency plane wave test case:
 
-k = 4 # wave number (q: select randomly or calculate?)
-w = 10 # frequency
-theta = 30 # angle
+
+w = 2*np.pi*10 # frequency
+v = 350 # speed of sound underground: 150 - 700 m/s
+k = w/(v*2*np.pi) 
+theta = 0 # angle
 sensors = 50 # number of sensors, assume 2m distance between each
 totTime = 10 # total time in seconds
     
-k = np.abs(k)*np.cos(theta) 
+k_x = np.abs(k)*np.cos(theta)
+# add in k_y term
 
-wave = planeWave(k, w, theta, sensors, totTime) 
+wave = planeWave(k_x, w, theta, sensors, totTime) 
 
-plt.imshow(wave, cmap=plt.cm.get_cmap('RdBu_r'))
+plt.imshow(wave, aspect='auto', cmap=plt.cm.get_cmap('RdBu_r'))
 plt.colorbar()
 plt.xlabel('time')
 plt.ylabel('channel')
