439a34245ad53b4084133435eb21419936524dbf
Anuradha Trivedi <agt17@vt.edu>
2021-02-14 12:04:09 -0500

    changed x and k to be vectors, added comments

--- planeWave.py
+++ planeWave.py
@@ -10,32 +10,43 @@ import matplotlib.pyplot as plt
 
 def planeWave(k, w, theta, sensors, totTime):
     
-    # make x and k vectors for future cases (add this in)
+    '''
+    inputs:
+        k = wave number array, dimensions = 2x1 
+        w = angular frequency (2*pi*f), float
+        theta = angle in radians, float
+        sensors = number of sensors spaced 2m apart, int
+        totTime = total time in seconds, float    
+        
+    output:
+        wave = plane wave array, dimensions = (sensors)x(4*totTime*w/pi)
+    '''
     
-    x    = np.linspace(0, 2*sensors, sensors)
-    t    = np.linspace(0, totTime, 4*int(w/np.pi)*totTime)
-    wave = np.zeros((len(x), len(t)))
-    
-    
+    x      = np.zeros((sensors, 2))
+    x[:,0] = np.linspace(0, 2*sensors, sensors)
+    t      = np.linspace(0, totTime, 4*int(w/np.pi)*totTime)
+    wave   = np.zeros((len(x), len(t)))
+        
     for i in range(len(x)): # loop over rows (channel #)
-        wave[int(i),:] = np.cos(x[int(i)]*k - w*t)
+        wave[int(i),:] = np.cos(np.matmul(np.transpose(x[int(i)]), k)- w*t)
 
     return wave
               
 ## create single frequency plane wave test case:
 
-
-w = 2*np.pi*10 # frequency
-v = 350 # speed of sound underground: 150 - 700 m/s
-k = w/(v*2*np.pi) 
-theta = 0 # angle
-sensors = 50 # number of sensors, assume 2m distance between each
+w       = 2*np.pi*5 # frequency
+v       = 350 # speed of sound underground: 150 - 700 m/s
+theta   = np.pi/6 # angle
+sensors = 20 # number of sensors, assume 2m distance between each
 totTime = 10 # total time in seconds
-    
-k_x = np.abs(k)*np.cos(theta)
-# add in k_y term
 
-wave = planeWave(k_x, w, theta, sensors, totTime) 
+# save x and y component of wave number
+k    = w/(v*2*np.pi)      
+k_x  = np.abs(k)*np.cos(theta)
+k_y  = np.abs(k)*np.sin(theta)
+kArr = np.array((k_x, k_y))
+
+wave = planeWave(kArr, w, theta, sensors, totTime) 
 
 plt.imshow(wave, aspect='auto', cmap=plt.cm.get_cmap('RdBu_r'))
 plt.colorbar()
